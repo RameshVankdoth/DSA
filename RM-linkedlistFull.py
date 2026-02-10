25e78e9384ff1c96d7e81082f9c9509812369043
Ramesh <vankyrs@gmail.com>
2026-02-10 22:15:43 +0530

    Quick

--- RM-linkedlistFull.py
+++ RM-linkedlistFull.py
@@ -1,62 +1,134 @@
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+#     # def Printll(self):
+#     #     temp = self
+#     #     while temp:
+#     #         print(str(temp.head))
+#     #         temp = temp.next
+
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#     def InsertStart(self, data):
+#         new_node = Node(data)
+#         new_node.next = self.head
+#         self.head = new_node
+
+#     def Printll(self):
+#         temp = self.head
+#         while temp:
+#             print(str(temp.data))
+#             temp = temp.next
+
+#     def insertEnd(self, data):
+#         new_data = Node(data)
+#         temp = self.head
+#         if temp is None:
+#             temp = new_data
+#             return
+
+#         while temp.next:
+#             temp = temp.next
+
+#         temp.next = new_data
+
+#     def insertbetween(self, head, end):
+#         return
+
+#     # def printllrec(self, node=None):
+#     #     if node is None:  # base case
+#     #         return
+
+#     #     print(node.data, end="-->")
+#     #     self.printllrec(node.next)
+
+
+# # ll = Node(10)
+# # ll.next = Node(20)
+# # ll.next.next = Node(30)
+# # ll.next.next.next = Node(50)
+# # ll.Printll()
+# ll = LinkedList()
+# ll.InsertStart(5)
+# ll.InsertStart(10)
+# ll.Printll()
+# ll.insertEnd(12)
+# ll.Printll()
+# # ll.printllrec(5)
+
+
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-    # def Printll(self):
-    #     temp = self
-    #     while temp:
-    #         print(str(temp.head))
-    #         temp = temp.next
 
-
-class LinkedList:
+class linkedlist:
     def __init__(self):
         self.head = None
 
-    def InsertStart(self, data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
+    def Insertll(self, data):
+        nd = Node(data)
+        nd.next = self.head
+        self.head = nd
 
-    def Printll(self):
+    def printll(self):
         temp = self.head
         while temp:
-            print(str(temp.data))
+            print(str(temp.data), end="-->")
             temp = temp.next
 
-    def insertEnd(self, data):
-        new_data = Node(data)
+    def Insertllback(self, data):
+        d = Node(data)
         temp = self.head
         if temp is None:
-            temp = new_data
-            return
+            temp = d
 
         while temp.next:
             temp = temp.next
+        temp.next = d
 
-        temp.next = new_data
+    def insertafter(self, data, n):
+        nd = Node(data)
+        temp = self.head
 
-    def insertbetween(self, head, end):
-        return
+        while temp:
+            if temp.data == n:
+                nd.next = temp.next
+                temp.next = nd
+                return
+            temp = temp.next
 
-    # def printllrec(self, node=None):
-    #     if node is None:  # base case
-    #         return
+    def insertbefore(self, data, n):
+        temp = self.head
+        nd = Node(data)
+        if not temp:
+            temp = nd
+            return
+        if temp.data == n:
+            nd.next = temp
+            temp = nd
+        prev = None
+        while temp.next and temp.data != n:
+            prev = temp
+            temp = temp.next
+        if temp:
+            prev.next = nd
+            nd.next = temp
 
-    #     print(node.data, end="-->")
-    #     self.printllrec(node.next)
 
-
-# ll = Node(10)
-# ll.next = Node(20)
-# ll.next.next = Node(30)
-# ll.next.next.next = Node(50)
-# ll.Printll()
-ll = LinkedList()
-ll.InsertStart(5)
-ll.InsertStart(10)
-ll.Printll()
-ll.insertEnd(12)
-ll.Printll()
-# ll.printllrec(5)
+ll = linkedlist()
+ll.Insertll(5)
+ll.Insertll(10)
+ll.printll()
+ll.Insertllback(15)
+ll.printll()
+ll.insertafter(20, 5)
+ll.printll()
+ll.insertbefore(13, 5)
+ll.printll()
